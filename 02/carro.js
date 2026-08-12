b95c57e66d4318a1e6101eb6ca6f500a39b18f3c
Jaber <jaberc69@gmail.com>
2026-08-12 15:04:50 -0300

    Feito o de carros.

---
+++ 02/carro.js
@@ -0,0 +1,34 @@
+const carros = [{
+    marca : "Volkswagen",
+    modelo: "Polo",
+    ano: "2025",
+    cor:"preta",
+    quantidade_portas: "04",
+    automatico: false
+},
+{
+    marca : "Ford",
+    modelo: "Fusion",
+    ano: "2020",
+    cor:"branca",
+    quantidade_portas: "04",
+    automatico: true
+},
+{
+    marca : "General Motors",
+    modelo: "Spin",
+    ano: "2021",
+    cor:"Cinza",
+    quantidade_portas: "05",
+    automatico: false
+},
+{
+    marca : "Renault",
+    modelo: "Duster",
+    ano: "2022",
+    cor:"Prata",
+    quantidade_portas: "04",
+    automatico: true
+},
+];
+console.log(carros[2]);
