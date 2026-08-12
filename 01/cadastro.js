4ecb164ddb08945363c86906540edb6fe779cbb6
Jaber <jaberc69@gmail.com>
2026-08-12 11:09:44 -0300

    Exercício 01 feito.

---
+++ 01/cadastro.js
@@ -0,0 +1,19 @@
+const endereco = {
+    rua: "Alameda das Roseiras",
+    numero: "01",
+    complemento: "Edf. Recanto das Ecsórias, apt. 001",
+    bairro: "Itapuã",
+    cidade: "Salvador",
+    estado: "BA",
+    cep: "40.060-835",
+};
+
+const usuario = {
+    nome: "Jáber Campos",
+    email: "jaberc69@gmail.com",
+    telefone: "71 98894-7440",
+    data_nascimento: "22/06/1969",
+    endereco: endereco
+};
+
+console.log(usuario);
